30a94be1ac2a28f1d0f5c4232804412cc45a4fb0
yl1310 <141566900+yl1310@users.noreply.github.com>
2023-08-06 21:21:27 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+     strings: ["Web Designer", "Code Developer","Apps Designer","Color-maker"],
+     typeSpeed:50,
+     backSpeed:50,
+     loop:true,
+});
+$('.counter').countUp();
